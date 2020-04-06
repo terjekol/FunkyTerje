a1bf0f54a0bbac3d3d81a454145a3019e0464029
GET Academy <terje@kolderup.net>
2020-04-06 12:38:53 +0200

    Create algebraFunctions.js

---
+++ math/algebraFunctions.js
@@ -0,0 +1,35 @@
+/*
+Ledd:
+  - Primtallsfaktorisere 
+  - Slå sammen ax^n og bx^n i sum, også n=0
+  - Slå sammen like ledd til 2*ledd
+  - Trekke fra ledd på begge sider. 
+  - Flytte ledd til andre siden av ligningen
+  - Trekke ut felles faktor fra sum
+  - 
+
+
+Brøk 
+ - Bytte fortegn (gange teller og nevner med -1)
+ - Gjøre om til desimaltall
+ - Pluss, minus, gange, dele
+ - Trekke ut faktor fra teller (også -1?)
+ - Gange hele ligningen med det som er i nevneren
+ - Forkorte (like faktorer)
+
+Faktor
+ - Slå sammen konstantfaktorer i samme produkt
+ - Lage kvadrat av to like faktorer
+ - Gange ut sum med de andre faktorene (løse ut parentes)
+
+
+
+Ligning: 
+ - Bytte fortegn (gange begge sider med -1)
+
+
+    Usikker på disse: 
+    - ConvertComplexNumberToSumOfSimpleNumbersOperation
+    - ConvertComplexNumberInFactorToSumOfSimpleNumbersOperation
+    - ConvertComplexNumberToSumOfSimpleNumbersOperation
+*/

87609045b67854aaadc8ab2c2928b14a1419cf0b
GET Academy <terje@kolderup.net>
2020-04-08 13:55:27 +0200

    x

--- math/math2html.js
+++ math/math2html.js
@@ -1,35 +1,40 @@
-function createMenuHtml(options){
-    return Object.keys(options).map(f=>`
-        <button onclick="doMath('${f}')">${model.mathOperations[f].icon.replace(/\n/g,'<br/>')}</button>
+function createMenuHtml(options) {
+    return Object.keys(options).map(f => `
+        <button onclick="doMath('${f}')">${model.mathOperations[f].icon.replace(/\n/g, '<br/>')}</button>
     `).join('');
 }
 
-function showMathText(mathText) {
+function showMathText(mathText, highlight) {
     const tree = parseMathText(mathText);
-    document.getElementById('mathContent').innerHTML = createHtml(tree);
+    document.getElementById('mathContent').innerHTML = createHtml(tree, hightlight);
 }
 
-function createHtml(node) {
-    return node.value != undefined ? `<div>${node.value.trim()}</div>` : createNodeHtml(node);
+function createHtml(node, hightlight) {
+    return node.value != undefined ? `<div>${node.value.trim()}</div>` : createNodeHtml(node, highlight);
 }
 
-function createNodeHtml(node) {
+function createNodeHtml(node, highlight) {
     if (node.operator === '/') return `
         <div class="flex vertical">
-            ${createHtml(node.content[0])}
+            ${createHtml(node.content[0], highlight)}
             <div class="fraction">&nbsp;</div>
-            ${createHtml(node.content[1])}
+            ${createHtml(node.content[1], highlight)}
         </div>
         `;
-    if (node.content.length > 1) return `
+    if (node.content.length > 1 && '+-'.includes(node.operator)) return `
         <div class="flex">
-            ${node.content.map(n => createHtml(n)).join(`<div>${node.operator.trim()}</div>`)}
+            ${node.content.map(n => createHtml(n, highlight)).join(`<div>${node.operator.trim()}</div>`)}
+        </div>
+        `;
+    if (node.content.length > 1 && node.operator === '*') return `
+        <div class="flex">
+            ${node.content.map(n => createHtml(n, highlight)).join(`<div>${node.operator.trim()}</div>`)}
         </div>
         `;
     if (node.operator === '-' && node.content.length === 1) return `
         <div class="flex">
             <div>-</div>
-            ${createHtml(node.content[0])}
+            ${createHtml(node.content[0], highlight)}
         </div>
         `;
     console.error('cannot create HTML', node);
